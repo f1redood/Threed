d80ac9cad055d7844b12e641ebba0f69fec9b388
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-10 10:53:44 -0500

    Create Scene.js

---
+++ Scene.js
@@ -0,0 +1,11 @@
+export default class Scene {
+  constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    
+    this.ctx = this.canvas.getContext("2d");
+
+    document.body.appendChild(this.canvas)
+  }
+}
