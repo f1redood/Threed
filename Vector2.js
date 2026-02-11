2ce6e75ce971a74233a072c9edd724f5fd992b6b
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-11 10:10:07 -0500

    Update Vector2.js

--- Vector2.js
+++ Vector2.js
@@ -5,58 +5,47 @@ export default class Vector2 {
   
   add(v) {
     if (v instanceof Vector2) {
-      this.x += v.x;
-      this.y += v.y;
+      return new Vector2(this.x + v.x, this.y + v.y);
     } else {
-      this.x += v;
-      this.y += v;
+      return new Vector2(this.x + v, this.y + v);
     }
   }
 
   sub(v) {
     if (v instanceof Vector2) {
-      this.x -= v.x;
-      this.y -= v.y;
+      return new Vector2(this.x - v.x, this.y - v.y);
     } else {
-      this.x -= v;
-      this.y -= v;
+      return new Vector2(this.x - v, this.y - v);
     }
   }
 
   mul(v) {
     if (v instanceof Vector2) {
-      this.x *= v.x;
-      this.y *= v.y;
+      return new Vector2(this.x * v.x, this.y * v.y);
     } else {
-      this.x *= v;
-      this.y *= v;
+      return new Vector2(this.x * v, this.y * v);
     }
   }
 
   div(v) {
     if (v instanceof Vector2) {
-      this.x /= v.x;
-      this.y /= v.y;
+      return new Vector2(this.x / v.x, this.y / v.y);
     } else {
-      this.x /= v;
-      this.y /= v;
+      return new Vector2(this.x / v, this.y / v);
     }
   }
 
   pow(v) {
     if (v instanceof Vector2) {
-      this.x = Math.pow(this.x, v.x);
-      this.y = Math.pow(this.y, v.y);
+      return new Vector2(Math.pow(this.x, v.x), Math.pow(this.y, v.y));
     } else {
-      this.x = Math.pow(this.x, v);
-      this.y += Math.pow(this.y, v);
+      return new Vector2(Math.pow(this.x, v), Math.pow(this.y, v));
     }
   }
 
   rotate(x) {
     var u = this.x;
     var v = this.y;
-    this.x = (u * Math.cos(x) - v * Math.sin(x));
-    this.y = (u * Math.sin(x) + v * Math.cos(x));
+    return new Vector2(u * Math.cos(x) - v * Math.sin(x), u * Math.sin(x) + v * Math.cos(x));
   }
 }
