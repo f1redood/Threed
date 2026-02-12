ee79844c333a7b6be00c70ce1eaf453ada1f4e25
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-12 08:12:41 -0500

    Update Vector3.js

--- Vector3.js
+++ Vector3.js
@@ -1,60 +1,68 @@
 export default class Vector3 {
+  static ZERO = new Vector3(0, 0, 0);
+  
   constructor(x, y, z) {
     [this.x, this.y, this.z] = [x, y, z];
   }
   
   add(v) {
-    this.x += v.x;
-    this.y += v.y;
-    this.z += v.z;
-  }
-  add(x) {
-    this.x += x;
-    this.y += x;
-    this.z += x;
+    if (v instanceof Vector3) {
+      return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
+    } else {
+      return new Vector3(this.x + v, this.y + v, this.z + v);
+    }
   }
 
   sub(v) {
-    this.x -= v.x;
-    this.y -= v.y;
-    this.z -= v.z;
-  }
-  sub(x) {
-    this.x -= x;
-    this.y -= x;
-    this.z -= x;
+    if (v instanceof Vector3) {
+      return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z);
+    } else {
+      return new Vector3(this.x - v, this.y - v, this.z - v);
+    }
   }
 
   mul(v) {
-    this.x *= v.x;
-    this.y *= v.y;
-    this.z *= v.z;
-  }
-  mul(v) {
-    this.x *= x;
-    this.y *= x;
-    this.z *= x;
+    if (v instanceof Vector3) {
+      return new Vector3(this.x * v.x, this.y * v.y, this.z * v.z);
+    } else {
+      return new Vector3(this.x * v, this.y * v, this.z * v);
+    }
   }
 
   div(v) {
-    this.x /= v.x;
-    this.y /= v.y;
-    this.z /= v.z;
-  }
-  div(x) {
-    this.x /= x;
-    this.y /= x;
-    this.z /= x;
+    if (v instanceof Vector3) {
+      return new Vector3(this.x / v.x, this.y / v.y, this.z / v.z);
+    } else {
+      return new Vector3(this.x / v, this.y / v, this.z / v);
+    }
   }
 
   pow(v) {
-    this.x = Math.pow(this.x, v.x);
-    this.y = Math.pow(this.y, v.y);
-    this.z = Math.pow(this.z, v.z);
+    if (v instanceof Vector3) {
+      return new Vector3(Math.pow(this.x, v.x), Math.pow(this.y, v.y), Math.pow(this.z, v.z));
+    } else {
+      return new Vector3(Math.pow(this.x, v), Math.pow(this.y, v), Math.pow(this.z, v));
+    }
   }
-  pow(v) {
-    this.x = Math.pow(this.x, x);
-    this.y = Math.pow(this.y, x);
-    this.z = Math.pow(this.z, x);
+
+  rotateX(x) {
+    var u = this.x;
+    var v = this.y;
+    var w = this.z
+    return new Vector3(u, v * Math.cos(x) - w * Math.sin(x), w * Math.cos(x) + v * Math.sin(x));
+  }
+
+  rotateY(x) {
+    var u = this.x;
+    var v = this.y;
+    var w = this.z
+    return new Vector3(u * Math.cos(x) + w * Math.sin(x), v, w * Math.cos(x) - u * Math.sin(x));
+  }
+
+  rotateZ(x) {
+    var u = this.x;
+    var v = this.y;
+    var w = this.z
+    return new Vector3(u * Math.cos(x) - v * Math.sin(x), v * Math.cos(x) + u * Math.sin(x), w);
   }
 }
