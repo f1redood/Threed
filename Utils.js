6bd975718e171b47fb1829ac588eaa20b05defe3
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-12 13:44:53 -0500

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -3,12 +3,12 @@ import Vector4 from "https://f1redewd123.github.io/Threed/Vector4.js";
 export default class Utils {
   static createPerspectiveProjectionMatrix(scene) {
     var aspect = scene.canvas.width / scene.canvas.height;
-    var far = 100;
-    var near = 0.001;
+    var f = 100;
+    var n = 0.001;
     
     return new Vector4(
-      new Vector4(1 / aspect * Math.tan(far / 2), 0, 0, 0),
-      new Vector4(0, 1 / Math.tan(far / 2), 0, 0),
+      new Vector4(1 / aspect * Math.tan(f / 2), 0, 0, 0),
+      new Vector4(0, 1 / Math.tan(f / 2), 0, 0),
       new Vector4(0, 0, (f + n) / (n - f), -1),
       new Vector4(0, 0, (f * n * 2) / (n - f), 0)
     );
