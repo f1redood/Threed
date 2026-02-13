430c4315d3ee3316f01c1f10fea61fd33c627ba9
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-13 08:04:29 -0500

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,7 @@
+(async function() {
+  var vec2 = await import("https://f1redewd123.github.io/Threed/Vector2.js");
+  var vec3 = await import("https://f1redewd123.github.io/Threed/Vector3.js");
+  var vec4 = await import("https://f1redewd123.github.io/Threed/Vector4.js");
+
+  return [ vec2, vec3, vec4 ];
+})();
