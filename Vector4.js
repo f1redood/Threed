f1069b9c296dc76761b1ef63bd46958f2fa958ff
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-19 18:19:50 -0500

    Update Vector4.js

--- Vector4.js
+++ Vector4.js
@@ -7,6 +7,46 @@ export default class Vector4 {
     return new Vector4(vec3.x, vec3.y, vec3.z, w);
   }
 
+  add(v) {
+    if (v instanceof Vector4) {
+      return new Vector4(this.x + v.x, this.y + v.y, this.z + v.z, this.w + v.w);
+    } else {
+      return new Vector4(this.x + v, this.y + v, this.z + v, this.w + v);
+    }
+  }
+
+  sub(v) {
+    if (v instanceof Vector4) {
+      return new Vector4(this.x - v.x, this.y - v.y, this.z - v.z, this.w - v.w);
+    } else {
+      return new Vector4(this.x - v, this.y - v, this.z - v, this.w - v);
+    }
+  }
+
+  mul(v) {
+    if (v instanceof Vector4) {
+      return new Vector4(this.x * v.x, this.y * v.y, this.z * v.z, this.w * v.w);
+    } else {
+      return new Vector4(this.x * v, this.y * v, this.z * v, this.w * v);
+    }
+  }
+
+  div(v) {
+    if (v instanceof Vector4) {
+      return new Vector4(this.x / v.x, this.y / v.y, this.z / v.z, this.w / v.w);
+    } else {
+      return new Vector4(this.x / v, this.y / v, this.z / v, this.w / v);
+    }
+  }
+
+  pow(v) {
+    if (v instanceof Vector4) {
+      return new Vector4(Math.pow(this.x, v.x), Math.pow(this.y, v.y), Math.pow(this.z, v.z), Math.pow(this.w, v.w));
+    } else {
+      return new Vector4(Math.pow(this.x, v), Math.pow(this.y, v), Math.pow(this.z, v), Math.pow(this.w, v));
+    }
+  }
+
   mulMatrix(mat) {
     return new Vector4(
       this.x * mat.x.x + this.y * mat.y.x + this.z * mat.z.x + this.w * mat.w.x,
